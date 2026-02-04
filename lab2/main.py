8fc632c744f72b969ab7374103fd27ca88e8f189
Linus Markström <lmark@kth.se>
2026-02-04 16:11:15 +0100

    Given data creation

--- lab2/main.py
+++ lab2/main.py
@@ -6,6 +6,27 @@ import matplotlib.pyplot as plt
 def objective():
     pass
 
+def create_data():
+    classA = numpy.concatenate(
+        (numpy.random.randn(10, 2) * 0.2 + [1.5, 0.5],
+         numpy.random.randn(10, 2) * 0.2 + [-1.5, 0.5]))
+    classB = numpy.random.randn(20, 2) * 0.2 + [0.0, -0.5]
+    
+    inputs = numpy.concatenate((classA, classB))
+    targets = numpy.concatenate(
+        (numpy.ones(classA.shape[0]),
+         -numpy.ones(classB.shape[0])))
+    
+    N = inputs.shape[0] # Number of rows (samples)
+    
+    permute = list(range(N))
+    random.shuffle(permute)
+    inputs = inputs[permute, :]
+    targets = targets[permute]
+    
+
+# HINT: Use list comprehension
+# a = [ expr for x in seq ]
 
 start = None
 B = None
