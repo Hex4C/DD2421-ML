c636454dd304110c924c76f7199c7d6da980f50f
Linus Markström <lmark@kth.se>
2026-02-13 15:25:21 +0100

    Lab files
    Co-authored-by: Jesper Lindeberg <Hex4C@users.noreply.github.com>

--- lab2/main.py
+++ lab2/main.py
@@ -1,36 +1,132 @@
-import numpy , random , math 
-from scipy.optimize import minimize
+import random
+from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import minimize
 
 
-def objective():
-    pass
+# List of kernels
+def linear_kernel(x, y):
+    return np.dot(x, y)
 
-def create_data():
-    classA = numpy.concatenate(
-        (numpy.random.randn(10, 2) * 0.2 + [1.5, 0.5],
-         numpy.random.randn(10, 2) * 0.2 + [-1.5, 0.5]))
-    classB = numpy.random.randn(20, 2) * 0.2 + [0.0, -0.5]
-    
-    inputs = numpy.concatenate((classA, classB))
-    targets = numpy.concatenate(
-        (numpy.ones(classA.shape[0]),
-         -numpy.ones(classB.shape[0])))
-    
-    N = inputs.shape[0] # Number of rows (samples)
-    
-    permute = list(range(N))
-    random.shuffle(permute)
-    inputs = inputs[permute, :]
-    targets = targets[permute]
-    
+def poly_kernel(x, y):
+    return (np.dot(x,y) + 1)**7
 
-# HINT: Use list comprehension
-# a = [ expr for x in seq ]
+def rbf_kernel(x, y):
+    sigma = 0.1
+    return np.exp(-(np.linalg.norm(x - y)**2)/ (2 * (sigma**2)))
 
-start = None
-B = None
-XC = None
+kernel = linear_kernel
 
-ret = minimize(objective, start, bounds=B, constraints=XC)
-alpha = ret['x']
+
+class_a = np.concatenate(
+    (
+        np.random.randn(10, 2) * 0.2 + [1.5, -0.1],
+        np.random.randn(10, 2) * 0.2 + [-1.5, -0.1],
+    )
+)
+class_b = np.random.randn(20, 2) * 0.2 + [0.0, -0.5]
+
+inputs = np.concatenate((class_a, class_b))
+targets = np.concatenate((np.ones(class_a.shape[0]), -np.ones(class_b.shape[0])))
+
+n = inputs.shape[0]  # Number of rows (samples)
+
+permute = list(range(n))
+random.shuffle(permute)
+inputs = inputs[permute, :]
+targets = targets[permute]
+
+
+p_mat = np.zeros((n, n))
+
+for i in range(n):
+    for j in range(n):
+        p_mat[i][j] = targets[i] * targets[j] * kernel(inputs[i], inputs[j])
+
+
+def zerofun(alpha):
+    return np.dot(alpha, targets)
+
+
+def objective(alpha):
+    return 0.5 * alpha.T @ p_mat @ alpha - np.sum(alpha)
+
+
+start = np.zeros(n)
+
+XC = {"type": "eq", "fun": zerofun}
+
+C = None
+ret = minimize(objective, start, bounds=[(0, C) for _ in range(n)], constraints=XC)
+
+if ret['success']:
+    print("Solution found")
+else:
+    print("Solution could not be found (non lin sep)")
+alpha = ret["x"]
+sv = [(alpha[i], inputs[i], targets[i]) for i in range(n) if abs(alpha[i] > 10e-5)]
+
+
+def b_term():
+    sum = 0
+    for val in sv:
+        sum += val[0] * val[2] * kernel(val[1], sv[0][1])
+    return sum - sv[0][2]
+
+
+def ind(x, y, b):
+    sum = 0
+    for val in sv:
+        sum += val[0] * val[2] * kernel([x, y], val[1])
+    return sum - b
+
+
+b = b_term()
+
+plt.figure(figsize=(10, 8))
+plt.title("SVM Descision Boundary with Linear Kernel", fontsize=22)
+
+line1 = plt.plot(
+    [p[0] for p in class_a],
+    [p[1] for p in class_a],
+    "r.",
+    label="class A"
+)
+
+line2 = plt.plot(
+    [p[0] for p in class_b],
+    [p[1] for p in class_b],
+    "b.",
+    label="class B"
+)
+
+plt.axis("equal")
+
+xlim = 2.5
+ylim = 2.5
+
+xgrid = np.linspace(-xlim, xlim)
+ygrid = np.linspace(-ylim, ylim)
+
+grid = np.array([[ind(x, y, b) for x in xgrid] for y in ygrid])
+plt.contourf(xgrid, ygrid, grid, 100, cmap="RdBu", alpha=0.6)
+plt.colorbar(label="Decision Function Value")
+
+ct = plt.contour(
+    xgrid,
+    ygrid,
+    grid,
+    (-1.0, 0.0, 1.0),
+    colors=("blue", "black", "red"),
+    linewidths=(1, 3, 1),
+)
+plt.clabel(ct, inline=True, fontsize=8)
+plt.xlabel("$x_1$", fontsize=16)
+plt.ylabel("$x_2$", fontsize=16)
+
+lines = [Line2D([0], [0], color="black"), Line2D([0], [0], color="red", marker="o", linestyle="None"), Line2D([0], [0], color="blue", marker="o", linestyle="None")]
+plt.legend(lines,["Descision boundary", "class A", "class B"], fontsize=16)
+plt.tight_layout()
+plt.show()
+
--- lab2/readme.md
+++ lab2/readme.md
@@ -1,3 +1,47 @@
 # Lab 2
 
-## Assignment 1
+## Question 1 (Linear kernel)
+
+The optimizer (minimize func) is not able to find a solution when the two classes are not linearly seperable. As can be seen in the images below:
+
+Here a solution is found, as the data is linearly seprarable.
+![Linear kernel, solution](./images/ml_solution.png)
+
+
+Here no solution is found, as the data is not linearly seperable.
+![Linear kernel, solution](./images/ml_no_solution.png)
+
+## Question 2 (Non-linear kernel)
+
+squared poly kernel and RBF kernel with $\sigma = 1.0$
+
+![Poly squared kernel, solution](./images/ml_solution_poly.png)
+
+
+![RBF kernel sigma=1.0, solution](./images/ml_solution_rbf.png)
+
+## Question 3 (Paremters)
+
+For the poly kernel increasing the polynomial changes increases the complexity of the descison space (boundary). This means that we are effectively increasing the features the and shapes the SVM can become. This means that we're more prone to overfitting to the specific points. Which in short means we have a high variance but low bias. 
+
+high degree = low bias, high variance
+low degree = high bias, low variance
+
+Polynomial kernel with grad=7
+![Poly  kernel, complex](./images/ml_poly_complex.png)
+
+The same is true for the RBF but in the opposite direction. By allowing for smaller sigma we decrease the variance and get a tigher bound which is more prone to overfit the data points, as a wider variance will smooth the classification leading to more generlized result. 
+
+High sigma = high bias, low variance
+low sigma = low bias, high variance
+
+RBF kernel with variance=5:
+![RBF  kernel, complex](./images/ml_rbf_complex.png)
+
+## Question 4 (C penelty term)
+
+C sets the bounds for the resulting alpha values. A lower C value means that the model is more lenient towards noisy data whilst a high C value is more restricting and does not allow for as much slack. This means that for noisy data a low C value is optimal. For non noisy data a high C value is more optimal.
+
+## Question 5 ()
+
+You would like to match the selection of model (kernel) i.e. RBF, polynomial or linear based on the data. For complex data (more features/classes) this tend to result in more complex models. If the data is more noisy, the best option would instead be to allow more slack. Otherwise, we could overfit on the noise, if we just increase the model complexity, which will result in more generalisation.
